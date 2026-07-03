b70b70b32ea0cd353f8f173665107476ac773b66
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-07-03 19:55:59 +0200

    some comments

--- Tesi triennale lean.lean
+++ Tesi triennale lean.lean
@@ -1,4 +1,4 @@
-import «Tesi triennale lean».Basic
+-- import «Tesi triennale lean».Basic
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Prod
@@ -11,13 +11,14 @@ import Mathlib.CategoryTheory.ConcreteCategory.Basic
 import Mathlib.CategoryTheory.Limits.Types.Limits
 open CategoryTheory Limits
 
+#set_option autoImplicit false
 
 universe u
-variable (C : Type u) [Category C] [HasTerminal C] 
+variable (C : Type u) [Category C] [HasTerminal C]
 
 def HasNNO C [Category C] [HasTerminal C] :=
     ∃ N : C, ∃ zero : (⊤_ C) ⟶ N, ∃ s : N ⟶ N,
-    ∀ X : C, ∀ f : (⊤_ C) ⟶  X, ∀ g : X ⟶  X, 
+    ∀ X : C, ∀ f : (⊤_ C) ⟶  X, ∀ g : X ⟶  X,
     ∃ h : N ⟶  X, (zero ≫ h = f ∧ s ≫ h = h ≫ g) ∧
     (∀ h1 : N ⟶ X, ∀ h2 : N ⟶ X, (zero ≫ h1 = f ∧ s ≫ h1 = h1 ≫ g
     ∧ zero ≫ h2 = f ∧ s ≫ h2 = h2 ≫ g) → h1 = h2)
@@ -27,43 +28,38 @@ def HasNNO C [Category C] [HasTerminal C] :=
 example : HasTerminal (Type) := inferInstance
 
 class CategoryTheory.Category_with_NNO (C : Type u) [Category C]
-[HasTerminal C] where 
-N : C  
-zero : ⊤_ C ⟶ N 
-s : N ⟶ N 
+[HasTerminal C] where
+N : C
+zero : ⊤_ C ⟶ N
+s : N ⟶ N
 recursion {X : C} (f : ⊤_ C ⟶ X) (g : X ⟶ X) : N ⟶  X
 fac_zero {X : C} (f : ⊤_ C ⟶ X) (g : X ⟶ X) : zero ≫ recursion f g = f
 fac_succ {X : C} (f : ⊤_ C ⟶ X) (g : X ⟶ X) : s ≫ recursion f g = recursion f g ≫ g
 uniq {X : C} (f : ⊤_ C ⟶ X) (g : X ⟶ X) (h : N ⟶ X) (hyp_zero : zero ≫ h = f)
-              (hyp_succ : s ≫ h = h ≫ g) : h = recursion f g 
+              (hyp_succ : s ≫ h = h ≫ g) : h = recursion f g
 
-noncomputable def RecFun {X : Type} (f : (⊤_ Type) → X) (g : X → X) := 
-    fun n => match n with 
-    |0 => f ((terminal.from PUnit).hom PUnit.unit)
-    |Nat.succ n => g (RecFun f g n)
+noncomputable def RecFun {X : Type} (f : (⊤_ Type) → X) (g : X → X) : ℕ → X :=
+  fun n => match n with
+  |0 => f ((terminal.from PUnit).hom PUnit.unit)
+  |Nat.succ n => g (RecFun f g n)
 
-noncomputable instance : Category_with_NNO (Type) where 
+noncomputable instance : Category_with_NNO (Type) where
 N := Nat
-zero := TypeCat.ofHom fun (_ : ⊤_ Type) => Nat.zero 
-s := TypeCat.ofHom Nat.succ 
-recursion f g := TypeCat.ofHom (RecFun f g) 
-fac_zero f g := by sorry 
+zero := TypeCat.ofHom fun (_ : ⊤_ Type) => Nat.zero
+s := TypeCat.ofHom Nat.succ
+recursion f g := TypeCat.ofHom (RecFun f g)
+fac_zero f g := by sorry
 fac_succ f g := by sorry
 uniq {X} f g h hyp_zero hyp_succ := by
-  let (φ : Nat → X) := g.hom 
-  have h2 : ∀ (n : Nat), h n = (RecFun f g) n := by 
+  let φ : X → X := g.hom -- l'errore era nelle parentesi, se ora scrive  `φ : ℕ → X` ha un errore
+  have h2 : ∀ (n : Nat), h n = (RecFun f g) n := by
     intro n
     induction n with
-  
   | zero =>
-    unfold RecFun
-    simp
-    rw [← h1_zero]
+    rw [← hyp_zero]
     rfl
- 
   | succ n ih =>
-    simp 
-    unfold RecFun
-    rw [ih]
-  --apply DFunLike.ext
-
+    have : h.hom (n+1) = (↾Nat.succ ≫ h).hom n := by
+      exact types_congr_hom rfl (n + 1)
+    rw [hyp_succ] at this
+    simp [this, RecFun, ih]
